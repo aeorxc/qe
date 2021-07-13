771327d39b9af899fdb0a3fd16dc4313adb40507
ui906879 <gibran.afzal@rwe.com>
2021-07-13 17:30:09 +0100

    fix: use start excel - seems to be required for windows 10

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 setuptools.setup(
     name="qe",
-    version="1.0.3",
+    version="1.0.4",
     author="aeorxc",
     author_email="author@example.com",
     description="turn pandas dataframe into an excel instance on Windows",
